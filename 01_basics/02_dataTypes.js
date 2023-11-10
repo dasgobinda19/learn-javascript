424c57f4701db0fa757decf2effdc6a49890cf89
Gobinda Das <103523606+dasgobinda19@users.noreply.github.com>
2023-11-10 21:26:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3 + 3)  // we are useing nodejs, not browser
+
+let name ="Gobinda"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+
+console.log(typeof name);
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
