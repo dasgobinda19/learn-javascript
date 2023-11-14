59198bda4747b5ad037e28294c551c4f67a67ad2
Gobinda Das <103523606+dasgobinda19@users.noreply.github.com>
2023-11-14 08:12:32 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,42 @@
+// Covert in Number
+let score = null     //null is a type of object
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN     (NaN typeof => number)
+// "gobinda" => NaN
+// null => 0
+// true => 1
+// false => 0
+
+
+
+
+
+// Convert in boolean
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "gobinda" => true
+
+
+
+
+
+// Convert in String
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber);
